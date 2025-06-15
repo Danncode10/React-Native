d7525d230ac0ae2d9fd8c7d86842fbc055216338
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 10:23:34 +0800

    Refactor App component to use ImageBackground for displaying images

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,12 +1,17 @@
-import { View, Text, Image} from "react-native";
+import { View, Text, Image, ImageBackground} from "react-native";
 
 const logoImage = require('./assets/adaptive-icon.png')
 
 export default function App(){
   return(
     <View style={{flex:1, backgroundColor: "plum", padding:50}}>
-        {/* <Image source={logoImage} style={{width:300, height:300}} />
-        <Image source={{uri: 'https://picsum.photos/300'}} style={{width:300, height:300}} /> */}
+       
+        <ImageBackground source={{uri: 'https://picsum.photos/300'}} style={{flex: 1}}>
+            <Text>This is an Image</Text>
+        </ImageBackground>
+        
     </View>
   );
 }
+ {/* <Image source={logoImage} style={{width:300, height:300}} />
+        <Image source={{uri: 'https://picsum.photos/300'}} style={{width:300, height:300}} /> */}
