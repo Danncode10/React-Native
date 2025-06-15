f997408ca7fa92aed85488c04e3501628989e781
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 15:37:34 +0800

    Refactor App component to remove unused state and buttons, simplifying the layout

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,45 +1,16 @@
-// Exercise
 
-import { Button, View, StatusBar } from "react-native";
-import { useState } from "react";
+import { View, ActivityIndicator} from "react-native";
+
 
 export default function App(){
-
-  const [barText, setBarText] = useState('default');
-  const [isHidden, setIsHidden] = useState(false);
-  const [bgColor, setBgColor] = useState('lightblue');
-
   return(
-    <View style={{backgroundColor:bgColor, flex:1, padding:60}}>
-
-
-      <StatusBar barStyle={barText} hidden={isHidden}>
-
-      </StatusBar>
-
-
-      <Button title="light" onPress={() => {
-        setBarText("light-content")
-      }}/>
-      <Button title="dark" onPress={() => {
-        setBarText("dark-content")
-        }}/>
-      <Button title="hide"onPress={() => {
-         setIsHidden(true);
-        }}/>
-      <Button title="unHide"onPress={() => {
-         setIsHidden(false);
-        }}/>
-      <Button title="Dark Mode"onPress={() => {
-        setBgColor('black');
-        setBarText("light-content")
-        setIsHidden(false);
-        }}/>
-      <Button title="Light Mode"onPress={() => {
-        setBgColor('lightblue');
-        setBarText("dark-content")
-        setIsHidden(false);
-        }}/>
+    <View style={{backgroundColor: 'plum', flex:1, padding: 60}}>
+      <ActivityIndicator
+        size="large"
+        color='midnightblue'
+        animating={true}
+      
+      ></ActivityIndicator>
     </View>
   );
 }
