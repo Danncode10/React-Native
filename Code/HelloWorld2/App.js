e6c5eeee4b285212defebbd531c0b93b45be17c8
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 10:03:15 +0800

    Update App component to change text color for "Hello" in greeting

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -3,7 +3,7 @@ import { View, Text} from "react-native";
 export default function App(){
   return(
     <View style={{flex:1, backgroundColor: "plum", padding:50}}>
-        <Text>Hello World</Text>
+        <Text><Text style={{color: 'white'}}>Hello</Text> World</Text>
     </View>
   );
 }
