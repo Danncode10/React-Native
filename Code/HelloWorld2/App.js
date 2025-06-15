747ff43fba7ccf82fcb89bede6780a9d21d134ed
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 10:47:19 +0800

    Refactor App component to use ScrollView and update layout with ImageBackground

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,17 +1,28 @@
-import { View, Text, Image, ImageBackground} from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView} from "react-native";
 
 const logoImage = require('./assets/adaptive-icon.png')
 
 export default function App(){
   return(
-    <View style={{flex:1, backgroundColor: "plum", padding:50}}>
+    <View style={{flex:1, backgroundColor: "plum", padding:40}}>
+
+      <ScrollView>
        
-        <ImageBackground source={{uri: 'https://picsum.photos/300'}} style={{flex: 1}}>
-            <Text>This is an Image</Text>
-        </ImageBackground>
-        
+        <Image source={logoImage} style={{width:300, height:300}} />
+
+        <Text style={{textAlign:'justify'}}>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+        </Text>
+
+          <Image source={logoImage} style={{width:300, height:300}} />
+
+        </ScrollView>
     </View>
   );
 }
- {/* <Image source={logoImage} style={{width:300, height:300}} />
-        <Image source={{uri: 'https://picsum.photos/300'}} style={{width:300, height:300}} /> */}
