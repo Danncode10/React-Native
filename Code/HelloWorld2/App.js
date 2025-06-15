78ce1f817c904f1e3a273f9be010cc11e76a4e3e
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 10:18:41 +0800

    Add images to App component and remove greeting text

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,9 +1,12 @@
-import { View, Text} from "react-native";
+import { View, Text, Image} from "react-native";
+
+const logoImage = require('./assets/adaptive-icon.png')
 
 export default function App(){
   return(
     <View style={{flex:1, backgroundColor: "plum", padding:50}}>
-        <Text><Text style={{color: 'white'}}>Hello</Text> World</Text>
+        {/* <Image source={logoImage} style={{width:300, height:300}} />
+        <Image source={{uri: 'https://picsum.photos/300'}} style={{width:300, height:300}} /> */}
     </View>
   );
 }
