c3868712865f77a6ebddbf4912909cdd533bb9f0
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 15:14:16 +0800

    Refactor App component to use dynamic background color and add Dark/Light Mode buttons

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,3 +1,5 @@
+// Exercise
+
 import { Button, View, StatusBar } from "react-native";
 import { useState } from "react";
 
@@ -5,9 +7,10 @@ export default function App(){
 
   const [barText, setBarText] = useState('default');
   const [isHidden, setIsHidden] = useState(false);
+  const [bgColor, setBgColor] = useState('lightblue');
 
   return(
-    <View style={{backgroundColor: 'lightblue', flex:1, padding:60}}>
+    <View style={{backgroundColor:bgColor, flex:1, padding:60}}>
 
 
       <StatusBar barStyle={barText} hidden={isHidden}>
@@ -17,17 +20,25 @@ export default function App(){
 
       <Button title="light" onPress={() => {
         setBarText("light-content")
-        setIsHidden(false);
       }}/>
       <Button title="dark" onPress={() => {
         setBarText("dark-content")
-         setIsHidden(false);
         }}/>
       <Button title="hide"onPress={() => {
          setIsHidden(true);
         }}/>
       <Button title="unHide"onPress={() => {
          setIsHidden(false);
+        }}/>
+      <Button title="Dark Mode"onPress={() => {
+        setBgColor('black');
+        setBarText("light-content")
+        setIsHidden(false);
+        }}/>
+      <Button title="Light Mode"onPress={() => {
+        setBgColor('lightblue');
+        setBarText("dark-content")
+        setIsHidden(false);
         }}/>
     </View>
   );
