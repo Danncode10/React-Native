43a7d3476043734088c43e609e8e541aa10a582b
Dann <Lesterdannlopez7@gmail.com>
2025-06-15 15:43:30 +0800

    Replace ActivityIndicator with Alert button for improved user interaction

--- Code/HelloWorld2/App.js
+++ Code/HelloWorld2/App.js
@@ -1,16 +1,25 @@
 
-import { View, ActivityIndicator} from "react-native";
+import { View, Alert, Button} from "react-native";
 
 
 export default function App(){
   return(
     <View style={{backgroundColor: 'plum', flex:1, padding: 60}}>
-      <ActivityIndicator
-        size="large"
-        color='midnightblue'
-        animating={true}
-      
-      ></ActivityIndicator>
+      <Button title="Alert"
+        onPress={() =>
+          Alert.alert("Alert", "The data you submit is incorrect", [
+            {
+              text: "Edit",
+              onPress: () => console.log("Data Edited")
+            },
+            {
+              text: "Cancel",
+              onPress: () => console.log("Clicked Exit")
+            }
+          ])
+        }
+      />
+
     </View>
   );
 }
