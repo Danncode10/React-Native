faac472008d5cf2a2581b59604dd025151f13191
Dann <Lesterdannlopez7@gmail.com>
2025-06-16 09:02:22 +0800

    Refactor App component to improve layout and styling consistency

--- Code/Styling/App.js
+++ Code/Styling/App.js
@@ -3,15 +3,31 @@ import { View, Text , StyleSheet} from 'react-native';
 export default function App() {
   return (
    <View style={styles.container} >
+    <View style={[styles.box, styles.lightblueBg]}>
+      <Text>Lightblue</Text>
+    </View>
 
+    <View>
+      <Text>Lightgreen</Text>
+    </View>
    </View>
   );
     
 }
 
-const styles = StyleSheet.create({
-  container: {backgroundColor: 'plum', 
-              flex: 1,
-              padding: 60
-            }
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'plum', 
+    flex: 1,
+    padding: 60,
+    },
+  box:{
+    height: 100,
+    width: 100,
+    padding: 10
+  },
+  lightblueBg: {
+    backgroundColor: 'lightblue'
+  }
+  
 })
