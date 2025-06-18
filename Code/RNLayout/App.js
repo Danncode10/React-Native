aa966fee756c9930e08c46fb7fe3033269c84f98
Dann <Lesterdannlopez7@gmail.com>
2025-06-18 13:56:46 +0800

    Refactor App component layout and add new Div component for improved structure and styling

--- Code/RNLayout/App.js
+++ Code/RNLayout/App.js
@@ -32,71 +32,133 @@
 // });
 
 
-import { StyleSheet, Text, View } from 'react-native';
-import Box from './components/Box';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Box from './components/Box';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
 
-      <View style={[styles.c_style, styles.addBorder, styles.margin10]}>
-        <Box style={styles.margin10}>A</Box>
-        <Box style={{borderWidth: 0}}>C</Box>
-        <Box style={styles.margin10}>B</Box>
-      </View>
+//       <View style={[styles.c_style, styles.addBorder, styles.margin10]}>
+//         <Box style={styles.margin10}>A</Box>
+//         <Box style={{borderWidth: 0}}>C</Box>
+//         <Box style={styles.margin10}>B</Box>
+//       </View>
 
-      <View style={[styles.addBorder, styles.margin10, {flex: 1}]}>
-        <Box style={[styles.margin10, styles.i_style]}>I</Box>
+//       <View style={[styles.addBorder, styles.margin10, {flex: 1}]}>
+//         <Box style={[styles.margin10, styles.i_style]}>I</Box>
 
-        <Box style={[styles.margin10, styles.h_style]}>
+//         <Box style={[styles.margin10, styles.h_style]}>
 
-            <Box style={{flex: 0.2}}>D</Box>
-            <Box style={{flex: 0.2}}>E</Box>
-            <Box style={{flex: 0.2}}>F</Box>
-            <Box style={{flex: 0.2}}>G</Box>
+//             <Box style={{flex: 0.2}}>D</Box>
+//             <Box style={{flex: 0.2}}>E</Box>
+//             <Box style={{flex: 0.2}}>F</Box>
+//             <Box style={{flex: 0.2}}>G</Box>
           
-        </Box>
+//         </Box>
 
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     marginTop: 60,
+//     borderWidth: 6,
+//     borderColor: 'red',
+    
+//   },
+  
+//   addBorder: {
+//       borderColor: 'black',
+//       borderWidth: 5,
+//   },
+
+//   margin10: {
+//     margin: 10
+//   },
+
+//   c_style: {
+//     flexDirection: 'column',
+//     justifyContent: 'space-between',
+//     // width: 175,
+//     flex: 0.4,
+//   },
+
+
+//   i_style: {
+//     flex: 1
+//   },
+
+//   h_style: {
+//     flex: 6,
+//     flexDirection: 'column',
+//     justifyContent: 'space-evenly',
+//   },
+
+// });
+
+
+// Practice 2: 
+
+
+import {View, StyleSheet} from 'react-native';
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      {/* Top Box */}
+      <View style={[styles.addBorder, styles.topBox]}>
+        {/* Menu */}
+        <View style={[styles.addBorder, styles.upperTopBox]}>
+          <View style={[styles.addBorder, styles.childUpperTopBox]}></View>
+          <View style={[styles.addBorder, styles.childUpperTopBox]}></View>
+          <View style={[styles.addBorder, styles.childUpperTopBox]}></View>
+          <View style={[styles.addBorder, styles.childUpperTopBox]}></View>
+        </View>
+        <View style={[styles.addBorder, styles.lowerTopBox]}></View>
       </View>
+
+      {/* Bot Box */}
+      <View style={[styles.addBorder, styles.botBox]}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
+    backgroundColor: 'plum',
+    flex: 1, 
+    padding: 20,
+    borderBlockColor: 'red',
     marginTop: 60,
-    borderWidth: 6,
-    borderColor: 'red',
-    
   },
-  
   addBorder: {
-      borderColor: 'black',
-      borderWidth: 5,
+    borderWidth: 5,
   },
-
-  margin10: {
-    margin: 10
+  topBox: {
+    flex: 1,
+    margin: 5,
   },
+      upperTopBox:{
+        flex: 1,
+        margin: 5,
+        flexDirection: 'row'
+      },
+          childUpperTopBox: {
+            flex: 1,
+            margin: 5,
+          },
+      lowerTopBox: {
+        flex: 5,
+        margin: 5
+      },
 
-  c_style: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    // width: 175,
-    flex: 0.4,
-  },
-
-
-  i_style: {
-    flex: 1
-  },
-
-  h_style: {
-    flex: 6,
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-  },
-
+  botBox: {
+    flex: 1,
+    margin: 5
+  }
 });
