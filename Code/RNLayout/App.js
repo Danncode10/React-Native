684108a6d7446209aca86131489af228f87bc884
Dann <Lesterdannlopez7@gmail.com>
2025-06-22 13:57:28 +0800

    Refactor Box components in App to implement flexShrink for better responsiveness and adjust layout for improved organization

--- Code/RNLayout/App.js
+++ Code/RNLayout/App.js
@@ -4,10 +4,10 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{backgroundColor: '#8e9b00'}}>Box 1</Box>
-      <Box style={{backgroundColor: "#b65d1f"}}>Box 2</Box>
-      <Box style={{backgroundColor: "#1c4c56", flexBasis: 140, flex: 1}}>Box 3</Box>
-      <Box style={{backgroundColor: "#ab9156", height: 200}}>Box 4</Box>
+      <Box style={{backgroundColor: '#8e9b00', flexShrink: 1}}>Box 1</Box>
+      <Box style={{backgroundColor: "#b65d1f", flexShrink: 1}}>Box 2</Box>
+      <Box style={{backgroundColor: "#1c4c56"}}>Box 3</Box>
+      <Box style={{backgroundColor: "#ab9156"}}>Box 4</Box>
       <Box style={{backgroundColor: "#6b0803"}}>Box 5</Box>
       <Box style={{backgroundColor: "#1c4c56"}}>Box 6</Box>
       <Box style={{backgroundColor: "#b95f21"}}>Box 7</Box>
@@ -21,6 +21,9 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: 'red',
+    width: 300,
+    flexDirection: 'row',
+    alignItems: 'flex-start'
     
   },
 });
