a21e6094e83dee25584fa1e9dd92b3981ca9c212
Dann <Lesterdannlopez7@gmail.com>
2025-07-14 12:05:37 +0800

    feat: enhance App component with improved text styling and structure

--- Nativewind_Lesson/L1_Setup/App.js
+++ Nativewind_Lesson/L1_Setup/App.js
@@ -7,11 +7,16 @@ export default function App() {
 
   return (
     <View className="flex-1 items-center justify-center bg-white">
+
+      <Text className="text-xl font-semibold text-center text-blue-600 uppercase underline">
+        Welcome to My App!
+      </Text>
+
       
       <View className={`w-60 h-60 ${boxColor} items-center justify-center`}>
-        <Text className="text-red-500">Error</Text>
-        <Text className="text-green-600">Success</Text>
-        <Text className="text-gray-500">Secondary Info</Text>
+        <Text className="line-through text-xl text-red-500 font-bold">Error</Text>
+        <Text className="text-xl text-green-600 font-light">Success</Text>
+        <Text className="underline text-xl text-gray-500 ">Secondary Info</Text>
 
       </View>
 
