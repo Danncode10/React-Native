89fe385ba7acdc5b1bff08d784c3f6a583d0f9e4
Dann <Lesterdannlopez7@gmail.com>
2025-07-15 18:54:07 +0800

    feat: refactor ProfileCard component for improved layout and styling

--- Nativewind_Lesson/L1_Setup/components/ProfileCard.js
+++ Nativewind_Lesson/L1_Setup/components/ProfileCard.js
@@ -1,16 +1,29 @@
-import {View, Image} from 'react-native';
+import {View, Image, Text} from 'react-native';
 
-export function ProfileCard({
-    profileImg, name, school, course, quote
-}) {
-    
+export function ProfileCard({ profileImg, name, school, course, quote }) {
   return (
-    <View className='w-full h-[200px] bg-blue-100 border border-blue-800 rounded-lg' >
-        <View className='justify-center '> 
-            <Image source={profileImg}
-                className="w-32 h-32 rounded-full"
-            ></Image>
-        </View>
+    <View className="w-full bg-blue-100 border border-blue-800 rounded-lg p-4 items-center">
+      
+      {/* Profile Image */}
+      <Image
+        source={profileImg}
+        className="w-24 h-24 rounded-full mb-3"
+      />
+
+      {/* Name */}
+
+      <Text className='text-2xl font-bold color-blue-900'>{name}</Text>
+
+      {/* School and Course */}
+
+      <Text className='text-xl color-gray-800'>{school}</Text>
+      <Text className='text-md color-gray-800'>{course}</Text>
+
+      <View className='w-2/3 h-20 bg-green-100 mt-5 border border-green-600 rounded-md items-center justify-center'>
+        <Text className='font-bold' >"{quote}"</Text>
+      </View>
+
+      
     </View>
   );
 }
