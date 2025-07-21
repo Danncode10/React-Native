0406c890857430d3af60719b612c7b5565395101
Dann <Lesterdannlopez7@gmail.com>
2025-07-21 18:39:48 +0800

    feat: implement FeedCard component and update App to display news articles in a scrollable view

--- Nativewind_Lesson/L2_ReactNative/App.js
+++ Nativewind_Lesson/L2_ReactNative/App.js
@@ -1,16 +1,14 @@
 import "./global.css"
-import { Text } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+import { View, SafeAreaView, Text } from "react-native";
+import { FeedCard } from "./components/FeedCard";
 
 export default function App() {
   return (
-    <LinearGradient
-      colors={['#4c669f', '#3b5998', '#192f6a']}
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-    >
-      <Text className="text-xl font-bold text-white">
-        Welcome to Nativewind!
-      </Text>
-    </LinearGradient>
+      <View className="bg-blue-100 flex-1">
+        <SafeAreaView>
+           <Text>Test</Text>
+        </SafeAreaView>   
+      </View>
+       
   );
 }
---
+++ Nativewind_Lesson/L2_ReactNative/components/FeedCard.js
@@ -0,0 +1,17 @@
+import {View, Text, Image,} from 'react-native';
+
+export function FeedCard({
+    uri = "https://picsum.photos/100", 
+    title = "Title",
+    infoText = "This is the info Text",
+    onPress
+}){
+
+
+    return(
+        <View>
+            
+        </View>
+    );
+
+}
