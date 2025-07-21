180743d4b0a0ce706383b71780c1501422828cce
Dann <Lesterdannlopez7@gmail.com>
2025-07-21 12:55:26 +0800

    feat: refactor App component to use StyledLinearGradient and update dependencies for expo-linear-gradient

--- Nativewind_Lesson/L1_Setup/App.js
+++ Nativewind_Lesson/L1_Setup/App.js
@@ -1,18 +1,16 @@
-import "./global.css"
-import {View, SafeAreaView} from 'react-native';
-import {Menu} from './components/Menu.js'
-import { Body } from "./components/Body.js";
+import { Text } from 'react-native';
+import { styled } from 'nativewind';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import {Gradient} from "./components/Gradient.js"
+const StyledLinearGradient = styled(LinearGradient);
 
 export default function App() {
-  return(
-      <View className="flex-1 items-center bg-blue-200 px-4">
-        <SafeAreaView>
-          {/* <Menu />
-          <Body /> */}
-        </SafeAreaView>
-
-    </View>
+  return (
+    <StyledLinearGradient
+      className="flex-1 items-center justify-center"
+      colors={['#ffffff', '#000000']}
+    >
+      <Text className="text-white text-lg">Test</Text>
+    </StyledLinearGradient>
   );
 }
